85815cfd59d3c8fc4d8b84e9965a87cde2b9f871
Hyrox_ <61850271+TheHyrox@users.noreply.github.com>
2021-08-02 18:54:45 +0200

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,4 +1,4 @@
 @echo off
 ::cd %userprofile%/YTDL Console app/
 node index.js
-%SystemRoot%\explorer.exe "C:\Users\Hyrox\Documents\YTDL Console app\Download"
+%SystemRoot%\explorer.exe "C:\Users\USERNAME\Documents\YTDL Console app\Download"
